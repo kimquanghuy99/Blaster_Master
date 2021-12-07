5ebf6194a50533f7c4118d0bb48ce799b8ee7ab9
kimquanghuy99 <18520411@gm.uit.edu.vn>
2021-12-07 19:01:52 +0700

    add brick

---
+++ 20000.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="20000" tilewidth="16" tileheight="16" tilecount="216" columns="24">
+ <image source="Super_Mario_Bros3-main/Super_Mario_Bros3/textures/BM/20000.png" width="386" height="153"/>
+</tileset>
---
+++ Blaster_Master_-_NES_-_Map_-_Area_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Blaster_Master_-_NES_-_Map_-_Area_3" tilewidth="1524" tileheight="780" tilecount="1" columns="1">
+ <image source="Super_Mario_Bros3-main/Super_Mario_Bros3/textures/BM/Blaster_Master_-_NES_-_Map_-_Area_3.png" width="1524" height="780"/>
+</tileset>
