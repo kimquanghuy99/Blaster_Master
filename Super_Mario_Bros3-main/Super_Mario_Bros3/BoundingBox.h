b158901b4bc36909858caf9d4a3614f7e7380d4c
kimquanghuy99 <18520411@gm.uit.edu.vn>
2021-12-30 17:50:10 +0700

    FIX DF

--- Super_Mario_Bros3-main/Super_Mario_Bros3/BoundingBox.h
+++ Super_Mario_Bros3-main/Super_Mario_Bros3/BoundingBox.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "GameObject.h"
-#define NoCollisionObject_TYPE_NORMAL	1
-#define NoCollisionObject_TYPE_NUMBER_THREE	2
-#define NoCollisionObject_TYPE_BUSH_INTRO_SCENE	3
+#include "DefineAll.h"
+
+
 class CNoCollisionObject : public CGameObject
 {
 	bool isRender;
--- Super_Mario_Bros3-main/Super_Mario_Bros3/HeathPoint.h
+++ Super_Mario_Bros3-main/Super_Mario_Bros3/HeathPoint.h
@@ -2,9 +2,7 @@
 #include "GameObject.h"
 #include "DefineAll.h"
 
-#define NoCollisionObject_TYPE_NORMAL	1
-#define NoCollisionObject_TYPE_NUMBER_THREE	2
-#define NoCollisionObject_TYPE_BUSH_INTRO_SCENE	3
+
 class CSTATBAR : public CGameObject
 {
 	bool isRender;
